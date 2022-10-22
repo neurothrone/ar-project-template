c6d1cbffe5bd2b657d60eda08131ecaa5af83ca5
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-22 04:18:05 +0200

    Initial Commit

---
+++ AR-Project-Template/AR_Project_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_Project_TemplateApp.swift
+//  AR-Project-Template
+//
+//  Created by Zaid Neurothrone on 2022-10-22.
+//
+
+import SwiftUI
+
+@main
+struct AR_Project_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
